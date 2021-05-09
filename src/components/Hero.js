1ec8f748729b869fedce9e42e10eeffaa8752102
Victor Garcia Mestre <vgmestre@gmail.com>
2021-05-09 22:45:54 +0200

    added navbar styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,8 @@
 import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Hero = () => {
-  return <div>Hero component</div>
+  return <header className="hero"></header>
 }
 
 export default Hero
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,29 @@
 import React from "react"
 
 const Navbar = () => {
-  return <div>Navbar component</div>
+  return (
+    <nav className="navbar">
+      <a href="#" class="logo">
+        Logo
+      </a>
+      <ul>
+        <li>
+          <a href="#" class="active-button">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#">Games</a>
+        </li>
+        <li>
+          <a href="#">About Us</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Navbar
