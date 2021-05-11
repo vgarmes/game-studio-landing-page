8ce6bba979d08a7408be0f99b5c446ccc1cf724a
Victor Garcia Mestre <vgmestre@gmail.com>
2021-05-11 14:57:51 +0200

    created Games, Game and Title components

---
+++ src/components/Games.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Title from "./Title"
+import Game from "./Game"
+import { Link } from "gatsby"
+
+const Games = () => {
+  return <div></div>
+}
+
+export default Games
