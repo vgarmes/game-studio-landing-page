c7acedb729a98011061ba5fd992ddcad15787e44
Victor Garcia Mestre <vgmestre@gmail.com>
2021-05-11 14:43:27 +0200

    added content and styles to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,24 @@
 import React from "react"
+import socialLinks from "../constants/social_links"
 
 const Footer = () => {
-  return <div></div>
+  return (
+    <footer className="footer">
+      <div className="footer-links social-links">
+        {socialLinks.map(link => {
+          return (
+            <a href={link.url} key={link.id} className="social-link">
+              {link.icon}
+            </a>
+          )
+        })}
+      </div>
+      <h4>
+        &copy; {new Date().getFullYear()}
+        <span> Supercool Games</span>
+      </h4>
+    </footer>
+  )
 }
 
 export default Footer
