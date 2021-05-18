4257dba5477a661febafe752ecef289b67272425
Victor Garcia Mestre <vgmestre@gmail.com>
2021-05-19 00:43:30 +0200

    created basic game pages

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,8 @@
 import React from "react"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import ReviewsSlider from "./ReviewsSlider"
+import slugify from "slugify"
+import { Link } from "gatsby"
 
 const Game = ({
   platforms,
@@ -8,7 +12,26 @@ const Game = ({
   cover,
   release_date,
 }) => {
-  return <div>{title}</div>
+  const slug = slugify(title, { lower: true })
+  return (
+    <article className="game">
+      <GatsbyImage
+        image={getImage(cover.localFile)}
+        className="game-img"
+        alt={title}
+      />
+      <div className="game-info">
+        <Link to={`/${slug}`}>
+          <h3>{title}</h3>
+        </Link>
+        <h4>Relased: {release_date}</h4>
+        <p className="game-description">{description}</p>
+        <div className="game-reviews">
+          <ReviewsSlider reviews_data={reviews} />
+        </div>
+      </div>
+    </article>
+  )
 }
 
 export default Game
--- src/components/Games.js
+++ src/components/Games.js
@@ -5,11 +5,14 @@ import { Link } from "gatsby"
 
 const Games = ({ games, title }) => {
   return (
-    <div>
-      {games.map(game => {
-        return <Game key={game.strapiId} {...game} />
-      })}
-    </div>
+    <section className="section games">
+      <Title title={title} />
+      <div className="wrapper">
+        {games.map(game => {
+          return <Game key={game.strapiId} {...game} />
+        })}
+      </div>
+    </section>
   )
 }
 
