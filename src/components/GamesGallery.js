753da9592d6a4dc90b327d758348045b488b48ae
Victor Garcia Mestre <vgmestre@gmail.com>
2021-05-26 13:39:58 +0200

    renamed Games to GamesGallery

---
+++ src/components/GamesGallery.js
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from "react"
+import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io"
+import { graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import slugify from "slugify"
+import { Link } from "gatsby"
+
+const query = graphql`
+  {
+    allStrapiGame(
+      sort: { fields: release_date, order: DESC }
+      filter: { featured: { eq: true } }
+    ) {
+      nodes {
+        id
+        platforms {
+          id
+          name
+        }
+        reviews {
+          id
+          author
+          review
+          score
+        }
+        title
+        description
+        featured
+        cover {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+            }
+          }
+        }
+        strapiId
+        release_date
+        desktop_picture {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const GamesGallery = () => {
+  const data = useStaticQuery(query)
+  const games = data.allStrapiGame.nodes
+
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    const lastIndex = games.length - 1
+    if (index < 0) {
+      setIndex(lastIndex)
+    }
+    if (index > lastIndex) {
+      setIndex(0)
+    }
+  }, [index, games])
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1)
+    }, 5000)
+    return () => clearInterval(slider)
+  }, [index])
+
+  return (
+    <div className="featured-games-gallery-container">
+      <div className="item-container">
+        {games.map((game, gameIndex) => {
+          const { id, title, description, desktop_picture } = game
+          const image = getImage(desktop_picture.localFile)
+          const slug = slugify(title, { lower: true })
+
+          let position = "nextSlide"
+          if (gameIndex === index) {
+            position = "activeSlide"
+          }
+          if (
+            gameIndex === index - 1 ||
+            (index === 0 && gameIndex === games.length - 1)
+          ) {
+            position = "lastSlide"
+          }
+
+          return (
+            <article key={id} className={`gallery-item ${position}`}>
+              <GatsbyImage
+                image={image}
+                className="gallery-item__picture"
+                style={{ position: "absolute" }}
+                alt={title}
+              />
+
+              <div className="gallery-item__text">
+                <h3>{title}</h3>
+                <p>Short description</p>
+                <Link to={`/${slug}`} className="btn btn-game">
+                  See more
+                </Link>
+              </div>
+            </article>
+          )
+        })}
+      </div>
+
+      <div className="paddlenav">
+        <ul>
+          <li className="paddlenav-item">
+            <button
+              className="paddlenav-arrow paddlenav-arrow-previous"
+              onClick={() => setIndex(index - 1)}
+            >
+              <IoIosArrowDropleft />
+            </button>
+          </li>
+          <li className="paddlenav-item">
+            <button
+              className="paddlenav-arrow paddlenav-arrow-next"
+              onClick={() => setIndex(index + 1)}
+            >
+              <IoIosArrowDropright />
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default GamesGallery
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,21 @@
 import React from "react"
-import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
-import Games from "../components/Games"
+import About from "../components/About"
+import GamesGallery from "../components/GamesGallery"
+import GamesButton from "../components/GamesButton"
+import JoinUs from "../components/JoinUs"
 
 const Home = ({ data }) => {
-  const {
-    allStrapiGame: { nodes: games },
-  } = data
-
   return (
     <Layout>
       <Hero />
-      <Games title="featured games" games={games} />
+      <About />
+      <GamesGallery />
+      <GamesButton />
+      <JoinUs />
     </Layout>
   )
 }
-
-export const query = graphql`
-  {
-    allStrapiGame(
-      sort: { fields: release_date, order: DESC }
-      filter: { featured: { eq: true } }
-    ) {
-      nodes {
-        id
-        platforms {
-          id
-          name
-        }
-        reviews {
-          id
-          author
-          review
-          score
-        }
-        title
-        description
-        featured
-        cover {
-          localFile {
-            childImageSharp {
-              gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
-            }
-          }
-        }
-        strapiId
-        release_date
-      }
-    }
-  }
-`
 
 export default Home
