7917f357bc25a5ad8bad85cfd877aebc90531229
Victor Garcia Mestre <vgmestre@gmail.com>
2021-05-26 13:40:27 +0200

    created About component

---
+++ src/components/About.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <main className="section-content">
+      <div className="wrapper">
+        <h2 className="section__headline">
+          We turn bold ideas into great games
+        </h2>
+        <p className="section__text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id eos nulla
+          cupiditate est unde minima aliquam deleniti ea ut nam adipisci
+          molestias ducimus ipsa aspernatur maiores dolore, labore amet
+          quisquam!<br></br>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt fuga et
+          inventore eius reprehenderit itaque modi minus veniam, excepturi porro
+          corrupti officia? A impedit rem cumque facere dolorem aperiam eaque!
+        </p>
+      </div>
+    </main>
+  )
+}
+
+export default About
