d919cc7450aa8b452d69f56009a0fc57351d24bd
Victor Garcia Mestre <vgmestre@gmail.com>
2021-05-26 13:56:16 +0200

    added button

--- src/components/JoinUs.js
+++ src/components/JoinUs.js
@@ -1,11 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const JoinUs = () => {
   return (
     <div className="section-content">
       <div className="wrapper">
         <h2 className="section__headline">Join us!</h2>
-        <p className="section__text">We are always looking for talent.</p>
+        <p className="section__text">
+          We are always on the lookout for talent. See our current job openings!{" "}
+        </p>
+        <div className="cta-wrapper">
+          <Link to="/careers" className="btn btn-careers">
+            See job openings
+          </Link>
+        </div>
       </div>
     </div>
   )
