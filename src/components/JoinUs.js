691cbeeb1fa3d64b8a8c03fc87e15c64aee52bc4
Victor Garcia Mestre <vgmestre@gmail.com>
2021-05-26 13:39:21 +0200

    created Join Us component

---
+++ src/components/JoinUs.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const JoinUs = () => {
+  return (
+    <div className="section-content">
+      <div className="wrapper">
+        <h2 className="section__headline">Join us!</h2>
+        <p className="section__text">We are always looking for talent.</p>
+      </div>
+    </div>
+  )
+}
+
+export default JoinUs
