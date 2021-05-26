5a54ee5a6bc367823d7e55de3df8d29dcfe950dd
Victor Garcia Mestre <vgmestre@gmail.com>
2021-05-26 13:41:14 +0200

    removed About section from Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -30,22 +30,6 @@ const Hero = () => {
           </div>
         </div>
       </header>
-
-      <main className="section hero-about">
-        <div className="wrapper">
-          <h3>We turn bold ideas into great games</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id eos
-            nulla cupiditate est unde minima aliquam deleniti ea ut nam adipisci
-            molestias ducimus ipsa aspernatur maiores dolore, labore amet
-            quisquam!<br></br>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt fuga
-            et inventore eius reprehenderit itaque modi minus veniam, excepturi
-            porro corrupti officia? A impedit rem cumque facere dolorem aperiam
-            eaque!
-          </p>
-        </div>
-      </main>
     </>
   )
 }
