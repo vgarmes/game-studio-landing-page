897edf77d55627fa4156c1aa0a774e8b24004585
Victor Garcia Mestre <vgmestre@gmail.com>
2021-05-27 18:07:18 +0200

    added site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,11 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Supercool Games",
+    description: "Games developer in Copenhagen",
+    author: "@vgmestre",
+  },
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
