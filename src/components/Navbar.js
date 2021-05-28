af3baae0661eca47aa0bed3af8290111382dfc52
Victor Garcia Mestre <vgmestre@gmail.com>
2021-05-28 21:47:57 +0200

    added navbar links as object

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,12 @@ import { Link } from "gatsby"
 import logo from "../assets/images/logo_192x192.png"
 
 const Navbar = () => {
+  const navLinks = [
+    { title: "Home", url: "/" },
+    { title: "Games", url: "/games" },
+    { title: "About", url: "/about" },
+    { title: "Career", url: "/careers" },
+  ]
   return (
     <nav className="navbar">
       <Link to="/" className="logo-desktop">
@@ -12,39 +18,46 @@ const Navbar = () => {
         <img src={logo} alt="supercool games" />
       </Link>
       <ul>
-        <li>
-          <Link to="/" className="nav__link" activeClassName="active-button">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/games"
-            className="nav__link"
-            activeClassName="active-button"
-          >
-            Games
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/about"
-            className="nav__link"
-            activeClassName="active-button"
-          >
-            About
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/careers"
-            className="nav__link"
-            activeClassName="active-button"
-          >
-            Careers
-          </Link>
-        </li>
+        {navLinks.map(({ title, url }, index) => {
+          return (
+            <li key={index}>
+              <Link
+                to={url}
+                className="nav__link"
+                activeClassName="active-button"
+              >
+                {title}
+              </Link>
+            </li>
+          )
+        })}
       </ul>
+
+      <button class="nav-toggle" aria-label="toggle navigation">
+        <span class="hamburger"></span>
+      </button>
+      <div class="nav-phone">
+        <div id="nav__close-layer"></div>
+        <div class="nav__sidebar">
+          <ul class="nav__list">
+            <li class="nav__item">
+              <a class="nav__link" href="#about">
+                <h1>About me</h1>
+              </a>
+            </li>
+            <li class="nav__item">
+              <a class="nav__link" href="#portfolio">
+                <h1>Projects</h1>
+              </a>
+            </li>
+            <li class="nav__item">
+              <a class="nav__link" href="#contact">
+                <h1>Contact</h1>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   )
 }
