3f207a1a9f7f7cb0ae41eceeb263cada5c922308
Victor Garcia Mestre <vgmestre@gmail.com>
2021-06-03 14:11:20 +0200

    added styles for site responsiveness

--- src/components/TeamSection.js
+++ src/components/TeamSection.js
@@ -14,17 +14,19 @@ const TeamSection = () => {
           placeholder="blurred"
         />
         <div className="team-content">
-          <h2 className="section__headline">Our team</h2>
-          <p className="section__text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id eos
-            nulla cupiditate est unde minima aliquam deleniti ea ut nam adipisci
-            molestias ducimus ipsa aspernatur maiores dolore, labore amet
-            quisquam!
-          </p>
-          <div className="cta-wrapper">
-            <Link to="/games" className="btn btn-cta">
-              Team members
-            </Link>
+          <div className="wrapper">
+            <h2 className="section__headline">Our team</h2>
+            <p className="section__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id eos
+              nulla cupiditate est unde minima aliquam deleniti ea ut nam
+              adipisci molestias ducimus ipsa aspernatur maiores dolore, labore
+              amet quisquam!
+            </p>
+            <div className="cta-wrapper">
+              <Link to="/games" className="btn btn-cta">
+                Team members
+              </Link>
+            </div>
           </div>
         </div>
       </div>
